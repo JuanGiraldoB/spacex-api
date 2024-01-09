e89a1574dbd3112eca803b04d2cb11981cdbbf8c
JuanGiraldoB <jugiraldob@unal.edu.co>
2024-01-09 07:25:14 -0500

    updated tests

--- spacex_data/tests.py
+++ spacex_data/tests.py
@@ -1,8 +1,41 @@
+from datetime import datetime
 from django.test import TestCase
-from .views import add_two_numbers
+from .utils import fetch_spacex_launches, categorize_launches, convert_unix_to_ymd
 
+class SpaceXTests(TestCase):
+    def test_fetch_spacex_launches(self):
+        # Test whether fetch_spacex_launches returns a valid response
+        response = fetch_spacex_launches()
+        self.assertIsNotNone(response)
+        self.assertIsInstance(response, list)
 
-class TestExample(TestCase):
+    def test_categorize_launches(self):
+        # Prepare test data
+        test_launches = [
+            {'success': True, 'upcoming': False},
+            {'success': False, 'upcoming': False},
+            {'success': True, 'upcoming': True},
+        ]
+        categorized = categorize_launches(test_launches)
 
-    def test_add_two_numbers(self):
-        self.assertEqual(add_two_numbers(2,2), 4)
+        # Check if categorization is done correctly
+        self.assertIn('successful', categorized)
+        self.assertIn('failed', categorized)
+        self.assertIn('upcoming', categorized)
+        self.assertIsInstance(categorized['successful'], list)
+        self.assertIsInstance(categorized['failed'], list)
+        self.assertIsInstance(categorized['upcoming'], list)
+
+    def test_convert_unix_to_ymd(self):
+        # Prepare test data
+        test_launches = {
+            'successful': [
+                {'date_unix': 1640995200},
+            ]
+        }
+        convert_unix_to_ymd(test_launches)
+
+        # Check if conversion is done correctly
+        for launch in test_launches.values():
+            for launch_detail in launch:
+                self.assertIsInstance(launch_detail['date_unix'], datetime)
